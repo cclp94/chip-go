035188fdd341c558d60a987e650296e2264e5dab
Caio Paiva <cclp94@gmail.com>
2024-02-06 22:28:45 -0500

    More instructions and better decoding

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "time"
+	"fmt"
+	"os"
+	"sync/atomic"
+	"time"
 )
 
 
@@ -44,92 +45,221 @@ func registerFont(memory []byte) {
 }
 
 
-func chip8(memory []byte) (func(), func()) {
+func getNibbleAt(value uint16, at int) uint16 {
+ return uint16((value & (0xf000 >> (at))) >> ((at + 3) * 4))
+}
 
-  // var display [64][32]byte
-  var pc int = 0x200
-  //var l *uint8
-  //var stack []uint16
-  //var delayTimer *Timer = CreateTimer()
-  //var soundTimer *Timer = CreateTimer() 
-  // var registers [16]uint8
+func chip8(
+  memory []byte,
+  delayTimer *atomic.Int64,
+  soundTimer *atomic.Int64,
+  isLegacy bool,
+) {
+
+  var pc uint16 = 0x200
+  var l uint8
+  var stack []uint16
+
+  var V [16]uint8
+
+  fmt.Println(l)
 
   fetchInstruction := func () uint16 {
     instruction := uint16(memory[pc])<<8 | uint16(memory[pc+1])
-    fmt.Printf("fetch instruction =0x%04X\n", instruction)
     pc += 2
     return instruction
   }
 
-  decodeInstruction := func () {
+  decodeInstruction := func (instruction uint16) {
     // Binary mask first hex nibble
-    switch instruction & 0xf000 {
+    switch getNibbleAt(instruction, 0) {
     case 0x0:
-      println("decoded: 0")
+      fmt.Println("decoded: 0")
       switch instruction & 0x00ff {
       case 0xE0:
         // TODO Clear screen
       case 0xEE:
-        // TODO pop from stack and set PC
+        fmt.Println("Exec 00EE")
+        toPopAtIndex := len(stack) - 1
+        pc = stack[toPopAtIndex]
+        stack = stack[:toPopAtIndex]
       }
     case 0x1:
-      println("decoded: 1")
+      fmt.Println("Exec 1NNN")
       addr := instruction & 0x0fff
-      // TODO Set pc to addr
+      pc = uint16(addr)
     case 0x2:
-      println("decoded: 2")
+      fmt.Println("Exec 2NNN")
       addr := instruction & 0x0fff
-      // TODO Push PC to stack
-
-      // TODO Set PC to addr
+      stack = append(stack, uint16(pc))
+      pc = uint16(addr)
     case 0x3:
-      println("decoded: 3")
+      fmt.Println("Exec 3XNN")
+      registerIndex := getNibbleAt(instruction, 1)
+      NN := uint8(instruction & 0x00ff)
+      if V[registerIndex] == NN {
+        pc += 2
+      }
     case 0x4:
-      println("decoded: 4")
+      fmt.Println("Exec 4XNN")
+      registerIndex := getNibbleAt(instruction, 1)
+      NN := uint8(instruction & 0x00ff)
+      if V[registerIndex] != NN {
+        pc += 2
+      }
     case 0x5:
-      println("decoded: 5")
+      fmt.Println("Exec 5XY0")
+      X := getNibbleAt(instruction, 1)
+      Y := getNibbleAt(instruction, 2)
+      if V[X] == V[Y] {
+        pc += 2
+      }
     case 0x6:
-      println("decoded: 6")
+      fmt.Println("Exec 6XNN")
+      registerIndex := getNibbleAt(instruction, 1)
+      NN := instruction & 0x00ff
+      V[registerIndex] = uint8(NN)
     case 0x7:
-      println("decoded: 7")
+      fmt.Println("Exec 7XNN")
+      registerIndex := getNibbleAt(instruction, 1)
+      NN := instruction & 0x00ff
+      V[registerIndex] += uint8(NN)
     case 0x8:
-      println("decoded: 8")
+      fmt.Println("decoded: 8")
+      switch instruction & 0x000f {
+      case 0x0:
+        fmt.Println("Exec 8XY0")
+        X := getNibbleAt(instruction, 1)
+        Y := getNibbleAt(instruction, 2)
+        V[X] = V[Y]
+      case 0x1:
+        fmt.Println("Exec  8XY1")
+        X := getNibbleAt(instruction, 1)
+        Y := getNibbleAt(instruction, 2)
+        V[X] =  V[X] | V[Y]
+      case 0x2:
+        fmt.Println("Exec  8XY2")
+        X := getNibbleAt(instruction, 1)
+        Y := getNibbleAt(instruction, 2)
+        V[X] =  V[X] & V[Y]
+      case 0x3:
+        fmt.Println("Exec  8XY3")
+        X := getNibbleAt(instruction, 1)
+        Y := getNibbleAt(instruction, 2)
+        V[X] =  V[X] ^ V[Y]
+      case 0x4:
+        fmt.Println("Exec  8XY4")
+        X := getNibbleAt(instruction, 1)
+        Y := getNibbleAt(instruction, 2)
+        if V[X]+V[Y] > 255 {
+          V[15] = 1 
+        } else {
+          V[15] = 0
+        }
+        V[X] =  V[X] + V[Y]
+      case 0x5:
+        fmt.Println("Exec  8XY5")
+        X := getNibbleAt(instruction, 1)
+        Y := getNibbleAt(instruction, 2)
+        if V[X] > V[Y] {
+          V[15] = 1 
+        } else {
+          V[15] = 0
+        }
+        V[X] =  V[X] - V[Y]
+      case 0x6:
+        fmt.Println("Exec  8XY6")
+        X := getNibbleAt(instruction, 1)
+        Y := getNibbleAt(instruction, 2)
+        // Execution for original COSMAC VIP programs
+        if isLegacy {
+          V[X] = V[Y]
+        }
+        shiftedBit := V[X] & 1
+        V[X] >>= 1
+        V[15] = shiftedBit
+      case 0x7:
+        fmt.Println("Exec  8XY7")
+        X := getNibbleAt(instruction, 1)
+        Y := getNibbleAt(instruction, 2)
+        if V[Y] > V[X] {
+          V[15] = 1 
+        } else {
+          V[15] = 0
+        }
+        V[X] =  V[Y] - V[X]
+      case 0xe:
+        fmt.Println("Exec  8XYE")
+        X := getNibbleAt(instruction, 1)
+        Y := getNibbleAt(instruction, 2)
+        // Execution for original COSMAC VIP programs
+        if isLegacy {
+          V[X] = V[Y]
+        }
+        shiftedBit := V[X] & 128
+        V[X] <<= 1
+        V[15] = shiftedBit
+      }
     case 0x9:
-      println("decoded: 9")
+      fmt.Println("Exec 9XY0")
+      X := getNibbleAt(instruction, 1)
+      Y := getNibbleAt(instruction, 2)
+      if V[X] != V[Y] {
+        pc += 2
+      }
     case 0xa:
-      println("decoded: a")
+      fmt.Println("Exec ANNN")
+      NNN := instruction & 0x0fff
+      l = uint8(NNN)
     case 0xb:
-      println("decoded: b")
+      fmt.Println("decoded: b")
     case 0xc:
-      println("decoded: c")
+      fmt.Println("decoded: c")
     case 0xd:
-      println("decoded: d")
+      fmt.Println("decoded: d")
     case 0xe:
-      println("decoded: e")
+      fmt.Println("decoded: e")
     case 0xf:
-      println("decoded: f")
+      fmt.Println("decoded: f")
     }
   }
 
-  return fetchInstruction, decodeIsntruction
 
+  tick := time.Tick(2 * time.Millisecond)
+
+  for { 
+    instruction := fetchInstruction()
+    decodeInstruction(instruction)
+    <- tick
+  }
+}
+
+func parseArgs(args []string) (string, bool){
+  var filename string
+  var isLegacy bool
+
+  if (len(args) < 2) {
+    fmt.Println("USAGE: <rom-file> [--legacy]")
+    panic(1)
+  }
+  filename = args[1]
+
+  if len(args) == 3 && args[2] == "--legacy" {
+    isLegacy = true
+  }
+  return filename, isLegacy
 }
 func main() {
-  romArg := os.Args[1]
+  filename, isLegacy := parseArgs(os.Args)
 
   var memory [4096]byte
-
+  var delayTimer *atomic.Int64 = timer()
+  var soundTimer *atomic.Int64 = timer() 
 
   // Font runs from addr 050 to 09F
   registerFont(memory[0x50:])
-  registerRom(romArg, memory[0x200:])
-  tick, _ := time.ParseDuration("1.5ms")
-
-  for pc < len(memory) - 2 { 
-    instruction := fetchInstruction(&pc, memory[:])
-    decode(instruction)
-    pc += 2
-    time.Sleep(tick) 
-  }
+  registerRom(filename, memory[0x200:])
+  
+  chip8(memory[:], delayTimer, soundTimer, isLegacy)
 }
 
--- timer.go
+++ timer.go
@@ -1,35 +1,23 @@
 package main
 
 import (
-  "fmt"
-  "math"
-  "time"
+	"sync/atomic"
+	"time"
 )
 
 
-type Timer struct {
-  duration uint8
-  tickHz int
-}
-
-func CreateTimer() *Timer {
-  t := Timer{duration: math.MaxUint8, tickHz: 60}
-  return &t
-}
-
-func (t *Timer) Reset () {
-  t.duration = math.MaxUint8
-
-}
-
-
-func (t *Timer) Start() {
+func timer() *atomic.Int64  {
+  var timer atomic.Int64 
+  
   go func() {
-    tick := time.Duration(1000 / t.tickHz)
-    for t.duration > uint8(0) {
-      t.duration -= 1
-      time.Sleep(tick * time.Millisecond)
-      fmt.Println(t.duration)
+    tick := time.Tick(17 * time.Millisecond)
+    for {
+      if timer.Load() > 0 {
+        timer.Add(-1)
+      }
+      <- tick
     }
   }()
+  return &timer
 }
+
